0947e6eb5be488ddeb4fecbdba829cdff60d7058
João Paulo <jp_cbc@hotmail.com>
2019-08-04 21:50:54 -0300

    Pesquisa de pessoas

--- src/main/java/com/joaopaulo/controller/PessoaController.java
+++ src/main/java/com/joaopaulo/controller/PessoaController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.joaopaulo.model.Pessoa;
@@ -63,9 +65,18 @@ public class PessoaController {
 		pessoaRepository.deleteById(idpessoa);
 		
 		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
-		andView.addObject("pessoaobj", pessoaRepository.findAll());
+		andView.addObject("pessoas", pessoaRepository.findAll());
 		andView.addObject("pessoaobj", new Pessoa());
 		return andView;
+	}
+	
+	@PostMapping("**/pesquisarpessoa")
+	public ModelAndView pesquisar(@RequestParam("nomepesquisa") String nomepesquisa) {
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+		andView.addObject("pessoas", pessoaRepository.findPessoaByName(nomepesquisa));
+		andView.addObject("pessoaobj", new Pessoa());
+		return andView;
+		
 	}
 
 }
