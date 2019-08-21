6a5094cd29dd86173843c18596ad35b8bb6aae12
João Paulo <jp_cbc@hotmail.com>
2019-08-20 22:28:44 -0300

    Consumindo Web Service RESTful CEP

--- src/main/java/com/joaopaulo/model/Pessoa.java
+++ src/main/java/com/joaopaulo/model/Pessoa.java
@@ -34,6 +34,23 @@ public class Pessoa implements Serializable {
 	
 	@OneToMany(mappedBy = "pessoa", orphanRemoval = true, cascade = CascadeType.ALL)// salva, consulta remove tudo de uma vez
 	private List<Telefone> telefones;
+	
+	@NotEmpty(message = "CEP obrigatório")
+	private String cep;
+	
+	@NotEmpty(message = "Rua obrigatório")
+	private String rua;
+	
+	@NotEmpty(message = "Bairro obrigatório")
+	private String bairro;
+	
+	private String complemento;
+	
+	@NotEmpty(message = "Bairro cidade")
+	private String cidade;
+	
+	@NotEmpty(message = "Estado obrigatório")
+	private String uf;
 
 	public Long getId() {
 		return id;
@@ -65,6 +82,62 @@ public class Pessoa implements Serializable {
 
 	public void setIdade(String idade) {
 		this.idade = idade;
+	}
+
+	public List<Telefone> getTelefones() {
+		return telefones;
+	}
+
+	public void setTelefones(List<Telefone> telefones) {
+		this.telefones = telefones;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
 	}
 
 }
